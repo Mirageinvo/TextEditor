c8dbc3e959a2fc4a66db93d0a6cac15d4a24382f
Mirage <andrej_malinin@mail.ru>
2022-03-06 15:55:15 +0300

    main changed

--- Text_Editor/sources/main.cpp
+++ Text_Editor/sources/main.cpp
@@ -5,7 +5,12 @@
 int main()
 {
     TextEditor el;
+    el.teach("LoR.txt");
+    el.teach("LoR_1.txt");
+    el.teach("LoR_2.txt");
+    el.teach("LoR_3.txt");
     el.teach("Onegin.txt");
+
     el.fix_mist("test.txt");
     return 0;
 }
