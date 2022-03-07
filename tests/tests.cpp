33fd8d3f41d503d5b9243cd86a66ba9a9811779d
Mirage <andrej_malinin@mail.ru>
2022-03-07 17:33:22 +0300

    added more tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <fstream>
 #include "text_editor.hpp"
 #include "gtest/gtest.h"
 
@@ -41,6 +41,69 @@ TEST(Dictionary, GetBestWordTest)
         ASSERT_TRUE(s == ret);
     }
 }
+
+TEST(Dictionary, AddWordsTest)
+{
+    Dictionary tmp;
+    size_t dist, freq;
+    std::string s, ret, path = "../../texts_for_teaching/test_texts/1.txt";
+    std::vector<std::string> arr;
+    tmp.add_words(path);
+    std::ifstream in;
+    in.open(path);
+    assert(in.is_open());
+    if (in.is_open())
+    {
+        while (!in.eof()) {
+            in >> s;
+            arr.push_back(s);
+        }
+        in.close();
+    }
+    for (size_t i = 0; i < arr.size(); ++i) {
+        dist = 1e9;
+        freq = 0;
+        ASSERT_TRUE(tmp.in_table(s));
+        tmp.get_best_word(ret, dist, freq, s);
+        ASSERT_TRUE(s == ret);
+    }
+}
+
+TEST(TextEditor, FindBestWordTest)
+{
+    TextEditor tmp;
+    std::string s, path = "test_texts/1.txt";
+    std::vector<std::string> arr;
+    tmp.teach(path);
+    std::ifstream in;
+    in.open("../../texts_for_teaching/" + path);
+    assert(in.is_open());
+    if (in.is_open())
+    {
+        while (!in.eof()) {
+            in >> s;
+            arr.push_back(s);
+        }
+        in.close();
+    }
+    for (const auto& word : arr) {
+        s = "";
+        for (size_t j = 0; j < word.size() - 2; ++j) {
+            s += word[j];
+        }
+        ASSERT_TRUE(word == tmp.find_best_word(s));
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 int main(int argc, char *argv[])
