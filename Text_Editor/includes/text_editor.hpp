4376ab4321402bd6b51afc54dfbeb7b92da03d48
Mirage <andrej_malinin@mail.ru>
2022-03-18 17:45:54 +0300

    find_best_word() became private

--- Text_Editor/includes/text_editor.hpp
+++ Text_Editor/includes/text_editor.hpp
@@ -10,10 +10,10 @@ class TextEditor
     ~TextEditor();
     void teach(const std::string &path);
     void fix_mist(const std::string &path);
-    std::string find_best_word(const std::string &word) const;
 
   private:
     Dictionary *tables_;
+    std::string find_best_word(const std::string &word) const;
     std::string cut_word_(const std::string &tmp, size_t &it1, size_t &it2);
 };
 
