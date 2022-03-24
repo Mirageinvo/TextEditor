b6ffbf8f76b97e2cbca2ee143e705e367bdc9210
Mirage <andrej_malinin@mail.ru>
2022-03-24 19:30:12 +0300

    started working with templates

--- Text_Editor/includes/hash_table.hpp
+++ Text_Editor/includes/hash_table.hpp
@@ -4,22 +4,45 @@
 #include <string>
 #include <vector>
 
-class HashTable
-{
-  public:
-    explicit HashTable(size_t capacity = 500000);
+template<typename T>
+class HashTable {
+public:
+    explicit HashTable(uint32_t capacity = initialSize);
     ~HashTable();
-    void add(const std::string &word, unsigned number_of = 1);
+    void add(const T& word, uint32_t number_of = numOf);
+    bool in_table(const T& word) const;
+
+protected:
+    uint32_t capacity_;
+    std::pair<uint32_t, T>* hash_table_;
+
+private:
+    const uint32_t numOf = 1;
+    const uint32_t initialSize = 500000;
+    uint32_t num_of_added_f_;
+    uint32_t num_of_added_s_;
+    uint64_t get_hash_(std::string word, int type, uint64_t mod) const;
+    void hsh_resize_();
+};
+
+template<>
+class HashTable<std::string> {
+  public:
+      const uint32_t numOf = 1;
+      const uint32_t initialSize = 500000;
+    explicit HashTable(uint32_t capacity = initialSize);
+    ~HashTable();
+    void add(const std::string &word, uint32_t number_of = numOf);
     bool in_table(const std::string &word) const;
 
   protected:
-    size_t capacity_;
-    std::pair<unsigned, std::string> *hash_table_f_;
-    std::pair<unsigned, std::string> *hash_table_s_;
+    uint32_t capacity_;
+    std::pair<uint32_t, std::string> *hash_table_f_;
+    std::pair<uint32_t, std::string> *hash_table_s_;
 
   private:
-    size_t num_of_added_f_;
-    size_t num_of_added_s_;
+    uint32_t num_of_added_f_;
+    uint32_t num_of_added_s_;
     uint64_t get_hash_(std::string word, int type, uint64_t mod) const;
     void hsh_resize_();
 };
