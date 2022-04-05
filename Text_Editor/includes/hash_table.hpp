bd60005d25e608ef78226bfe21b07345533b0636
Mirage <andrej_malinin@mail.ru>
2022-04-05 17:47:41 +0300

    specialization added

--- Text_Editor/includes/hash_table.hpp
+++ Text_Editor/includes/hash_table.hpp
@@ -4,47 +4,44 @@
 #include <string>
 #include <vector>
 
-template<typename T>
+template <typename T>
 class HashTable {
-public:
-    explicit HashTable(uint32_t capacity = initialSize);
+   public:
+    explicit HashTable(uint32_t capacity = initialSize_);
     ~HashTable();
-    void add(const T& word, uint32_t number_of = numOf);
+    void add(const T& word, uint32_t number_of = numOf_);
     bool in_table(const T& word) const;
 
-protected:
+   private:
+    static const uint32_t numOf_ = 1;
+    static const uint32_t initialSize_ = 500000;
     uint32_t capacity_;
     std::pair<uint32_t, T>* hash_table_;
-
-private:
-    const uint32_t numOf = 1;
-    const uint32_t initialSize = 500000;
-    uint32_t num_of_added_f_;
-    uint32_t num_of_added_s_;
+    uint32_t num_of_added_;
     uint64_t get_hash_(std::string word, int type, uint64_t mod) const;
     void hsh_resize_();
 };
 
-template<>
+template <>
 class HashTable<std::string> {
-  public:
-      const uint32_t numOf = 1;
-      const uint32_t initialSize = 500000;
-    explicit HashTable(uint32_t capacity = initialSize);
+   public:
+    explicit HashTable(uint32_t capacity = initialSize_);
     ~HashTable();
-    void add(const std::string &word, uint32_t number_of = numOf);
-    bool in_table(const std::string &word) const;
+    void add(const std::string& word, uint32_t number_of = numOf_);
+    bool in_table(const std::string& word) const;
 
-  protected:
+   protected:
     uint32_t capacity_;
-    std::pair<uint32_t, std::string> *hash_table_f_;
-    std::pair<uint32_t, std::string> *hash_table_s_;
+    std::pair<uint32_t, std::string>* hash_table_f_;
+    std::pair<uint32_t, std::string>* hash_table_s_;
 
-  private:
+   private:
+    static const uint32_t numOf_ = 1;
+    static const uint32_t initialSize_ = 500000;
     uint32_t num_of_added_f_;
     uint32_t num_of_added_s_;
     uint64_t get_hash_(std::string word, int type, uint64_t mod) const;
     void hsh_resize_();
 };
 
-#endif // TEXT_EDITOR_HASH_TABLE
+#endif  // TEXT_EDITOR_HASH_TABLE
