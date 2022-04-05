3fc54d2d017aae2c1ed394ae26fea847d9874bd8
Mirage <andrej_malinin@mail.ru>
2022-04-05 17:47:01 +0300

    tests fixed

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,40 +1,35 @@
-#include "text_editor.hpp"
-#include "gtest/gtest.h"
 #include <fstream>
 
-const size_t kItsF = 1e3;
-const size_t kItsS = 1e2;
+#include "gtest/gtest.h"
+#include "text_editor.hpp"
 
-TEST(HashTable, InTableTest)
-{
-    HashTable tmp;
+const uint32_t kItsF = 1e3;
+const uint32_t kItsS = 1e2;
+
+TEST(HashTable, InTableTest) {
+    HashTable<std::string> tmp;
     std::string s;
-    for (size_t i = 0; i < kItsF; ++i)
-    {
+    for (uint32_t i = 0; i < kItsF; ++i) {
         s += static_cast<char>(static_cast<unsigned>('a') + (i % 26));
         ASSERT_FALSE(tmp.in_table(s));
     }
 }
 
-TEST(HashTable, AddTest)
-{
-    HashTable tmp;
+TEST(HashTable, AddTest) {
+    HashTable<std::string> tmp;
     std::string s;
-    for (size_t i = 0; i < kItsF; ++i)
-    {
+    for (uint32_t i = 0; i < kItsF; ++i) {
         s += static_cast<char>(static_cast<unsigned>('a') + (i % 26));
         tmp.add(s);
         ASSERT_TRUE(tmp.in_table(s));
     }
 }
 
-TEST(Dictionary, GetBestWordTest)
-{
+TEST(Dictionary, GetBestWordTest) {
     Dictionary tmp;
-    size_t dist, freq;
+    uint32_t dist, freq;
     std::string s, ret;
-    for (size_t i = 0; i < kItsS; ++i)
-    {
+    for (uint32_t i = 0; i < kItsS; ++i) {
         dist = 1e9;
         freq = 0;
         s += static_cast<char>(static_cast<unsigned>('a') + (i % 26));
@@ -45,27 +40,23 @@ TEST(Dictionary, GetBestWordTest)
     }
 }
 
-TEST(Dictionary, AddWordsTest)
-{
+TEST(Dictionary, AddWordsTest) {
     Dictionary tmp;
-    size_t dist, freq;
+    uint32_t dist, freq;
     std::string s, ret, path = "../../Text_Editor/texts_for_teaching/test_text/0.txt";
     std::vector<std::string> arr;
     tmp.add_words(path);
     std::ifstream in;
     in.open(path);
     ASSERT_TRUE(in.is_open());
-    if (in.is_open())
-    {
-        while (!in.eof())
-        {
+    if (in.is_open()) {
+        while (!in.eof()) {
             in >> s;
             arr.push_back(s);
         }
         in.close();
     }
-    for (size_t i = 0; i < arr.size(); ++i)
-    {
+    for (uint32_t i = 0; i < arr.size(); ++i) {
         dist = 1e9;
         freq = 0;
         ASSERT_TRUE(tmp.in_table(s));
@@ -74,8 +65,7 @@ TEST(Dictionary, AddWordsTest)
     }
 }
 
-TEST(TextEditor, EndToEndTestFirst)
-{
+TEST(TextEditor, EndToEndTestFirst) {
     TextEditor tmp;
     std::string sf, ss;
     tmp.teach("LoR.txt");
@@ -89,10 +79,8 @@ TEST(TextEditor, EndToEndTestFirst)
     in_ans.open("../../fixed_texts/test_texts/1_ans.txt");
     assert(in.is_open());
     assert(in_ans.is_open());
-    if (in.is_open() && in_ans.is_open())
-    {
-        while (!in.eof() && !in_ans.eof())
-        {
+    if (in.is_open() && in_ans.is_open()) {
+        while (!in.eof() && !in_ans.eof()) {
             std::getline(in, sf);
             std::getline(in_ans, ss);
             ASSERT_TRUE(sf == ss);
@@ -104,8 +92,7 @@ TEST(TextEditor, EndToEndTestFirst)
     ASSERT_TRUE(remove("../../fixed_texts/test_texts/1.txt") == 0);
 }
 
-TEST(TextEditor, EndToEndTestSecond)
-{
+TEST(TextEditor, EndToEndTestSecond) {
     TextEditor tmp;
     std::string sf, ss;
     tmp.teach("LoR.txt");
@@ -119,10 +106,8 @@ TEST(TextEditor, EndToEndTestSecond)
     in_ans.open("../../fixed_texts/test_texts/2_ans.txt");
     assert(in.is_open());
     assert(in_ans.is_open());
-    if (in.is_open() && in_ans.is_open())
-    {
-        while (!in.eof() && !in_ans.eof())
-        {
+    if (in.is_open() && in_ans.is_open()) {
+        while (!in.eof() && !in_ans.eof()) {
             std::getline(in, sf);
             std::getline(in_ans, ss);
             ASSERT_TRUE(sf == ss);
@@ -134,8 +119,7 @@ TEST(TextEditor, EndToEndTestSecond)
     ASSERT_TRUE(remove("../../fixed_texts/test_texts/2.txt") == 0);
 }
 
-TEST(TextEditor, EndToEndTestThird)
-{
+TEST(TextEditor, EndToEndTestThird) {
     TextEditor tmp;
     std::string sf, ss;
     tmp.teach("LoR.txt");
@@ -149,10 +133,8 @@ TEST(TextEditor, EndToEndTestThird)
     in_ans.open("../../fixed_texts/test_texts/3_ans.txt");
     assert(in.is_open());
     assert(in_ans.is_open());
-    if (in.is_open() && in_ans.is_open())
-    {
-        while (!in.eof() && !in_ans.eof())
-        {
+    if (in.is_open() && in_ans.is_open()) {
+        while (!in.eof() && !in_ans.eof()) {
             std::getline(in, sf);
             std::getline(in_ans, ss);
             ASSERT_TRUE(sf == ss);
@@ -164,8 +146,7 @@ TEST(TextEditor, EndToEndTestThird)
     ASSERT_TRUE(remove("../../fixed_texts/test_texts/3.txt") == 0);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
